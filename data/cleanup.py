02edde85acb3bee1bdd6a44c72ccbdaf5c44f616
Christophorus Khosasih <christophorus.khosasih@gmail.com>
2025-06-19 11:19:51 -0700

    what message am i supposed to put lmao

---
+++ data/cleanup.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+class TeamCleanup:
+    def __init__(self):
+        self.team_raw = pd.read_csv('raw_data/team_raw.csv')
+        self.team_raw.drop(columns=['TEAM_NAME','FG_PCT','FG3_PCT','FT_PCT','REB','PF','PFD'], inplace=True)
+        self.team_raw.drop(self.team_raw.columns[-27:], axis=1, inplace=True)
+        self.team_raw['row_num'] = self.team_raw.groupby('GAME_ID').cumcount() + 1
+
+        df_pivot = self.team_raw.pivot(index='GAME_ID', columns='row_num')
+        df_pivot.columns = [f"{col}_{num}" for col, num in df_pivot.columns]
+        self.team = df_pivot.reset_index()
+        
+    
+df = TeamCleanup().team
+
+print(df.columns)
