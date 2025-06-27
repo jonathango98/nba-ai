9fe6f831d56360a71fd53c0937188f3f7963e575
Jeremiah Harsojo <jeremiahn.harsojo@gmail.com>
2025-06-26 23:35:34 -0700

    Improved Documentation and Readability
    Added comments

--- data/process.py
+++ data/process.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+#Set DEBUG to False to save data
 DEBUG = True
 DEBUG = False
 
@@ -19,11 +19,12 @@ class Process:
         years = self.floor_year.apply(self.start_year, axis = 1)
         self.floor_year["season_year"] = years
 
+        #Filter games into discrete seasons, stores in a dictionary
         self.seasons = {}
         for i in range(OLDEST_SEASON, NEWEST_SEASON + 1):
             self.seasons[i] = self.floor_year[self.floor_year["season_year"] == i]
         
-
+        #Scrapes data from the newest season to get the relationship between team ids and team names
         iter_frame = self.seasons[NEWEST_SEASON].itertuples()
         temp = {}
         while(len(temp) < TEAM_AMOUNT):
@@ -32,26 +33,26 @@ class Process:
                 temp[row.team_id_home] = row.team_home
             if row.team_id_away not in temp.keys():
                 temp[row.team_id_away] = row.team_away
-
         self.teams = pd.DataFrame([{'team_id': k, 'team_name': v} for k, v in temp.items()])
         self.teams = self.teams.sort_values(by = 'team_name')
+
+
     def start_year(self,row):
         return int(row["season_year"][:4])
 
-
+#Saves the dataframes into discrete CSVs when DEBUG is False
 result = Process()
 sorted = result.sorted_data
 floor_year = result.floor_year
 teams = result.teams
+
+#Set DEBUG at top of program
 if not DEBUG:
     sorted.to_csv('clean_data/subdata/sort.csv', index= False)
     floor_year.to_csv('clean_data/subdata/floor_year.csv', index= False)
     teams.to_csv('clean_data/subdata/teams.csv', index = False)
     for key, dataframe in result.seasons.items():
         dataframe.to_csv(f"clean_data/subdata/seasons/season_{key}.csv", index = False)
-    
-    # season_2023.to_csv('clean_data/subdata/season_2023.csv', index= False)
 else:
     print(f"Sorted datatypes: \n {sorted.dtypes} \n")
     print(f"Floor_year datatypes: \n {floor_year.dtypes} \n")
-    # print(f"Season_2023 datatypes: \n {season_2023.dtypes} \n")
